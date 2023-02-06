36fe060b3a7021184943994137df93a278d6d35e
lingen <1762861715@qq.com>
2023-02-06 10:54:24 +0800

    更新web

--- static/web/assets/javascript-a7b0b1f4.js
+++ static/web/assets/javascript-a7b0b1f4.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-891bf910.js";import"./index-106bd210.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-2979ee4a.js";import"./index-85a1e30d.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.34.1(547870b6881302c5b4ff32173c16d06009e3588f)
  * Released under the MIT license
