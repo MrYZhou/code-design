2cc7219b8fe6c194773efd37b9d3514fce8c72eb
larry <1762861715@qq.com>
2023-02-08 23:11:04 +0800

    提交

--- main.py
+++ main.py
@@ -8,11 +8,10 @@ app = FastAPI()
 
 Init.do(app)
 
-
 @app.get("/")
 async def index():
-    return FileResponse("static/web/index.html")
+    return ''
 
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True, workers=1)
+    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=False, workers=1)
