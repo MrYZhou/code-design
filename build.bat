1a7c7b041b0b56ce49b8b99d4c4c4c602f964926
larry <1762861715@qq.com>
2023-02-08 23:10:45 +0800

    构建脚本

--- build.bat
+++ build.bat
@@ -1,2 +1,10 @@
-cd static && rmdir /s /q web
-cd ../web && pnpm i && pnpm build
+pyinstaller --onefile --noconfirm  ^
+--windowed --clean --name "pyparser" ^
+--add-data "./static;static/" ^
+--add-data "./main.py;."  "./main.py"
+
+
+@REM pyinstaller  --onedir --console --name "pyparser"  --clean ^
+@REM     --add-data "./static;static/" ^
+@REM     --add-data "./main.py;." "./main.py" ^
+  
